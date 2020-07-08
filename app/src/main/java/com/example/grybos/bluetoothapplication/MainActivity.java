eafee7c2dc9b9328eeb3a3301c3e9b927c1c75af
Paweł <pawelgr7@gmail.com>
2020-07-08 10:20:47 +0200

    ver.1.1

--- app/src/main/java/com/example/grybos/bluetoothapplication/MainActivity.java
+++ app/src/main/java/com/example/grybos/bluetoothapplication/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.grybos.bluetoothapplication;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,16 +14,24 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -47,6 +58,15 @@ public class MainActivity extends AppCompatActivity {
     private IntentFilter scanFilter;
     private Receivers receivers;
     private LocationManager gps_manager;
+    private Button connect;
+    private Button send;
+    private TextView status;
+    private ProgressDialog progressDialog;
+    private Communication communication;
+
+    //serwerowe
+    private static final String APP_NAME = "Gryboś_chat";
+    private static final UUID MY_UUID = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +78,16 @@ public class MainActivity extends AppCompatActivity {
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         gps_manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        getSupportActionBar().setTitle("Nazwa urządzenia: " + bluetoothAdapter.getName());
+
+        connect = findViewById(R.id.connect);
+        send = findViewById(R.id.send);
+        status = findViewById(R.id.status);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("server listening...");
+        progressDialog.setCancelable(false);
 
         checkIfBluetooth();
 
@@ -146,7 +176,6 @@ public class MainActivity extends AppCompatActivity {
                             }else {
 
                                 devices.clear();
-
                                 bluetoothAdapter.startDiscovery();
 
                             }
@@ -171,6 +200,27 @@ public class MainActivity extends AppCompatActivity {
 
                         break;
                 }
+
+            }
+        });
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Server server = new Server();
+                server.start();
+                progressDialog.show();
+
+            }
+        });
+
+        listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Client client = new Client(devices.get(i));
+                client.start();
 
             }
         });
@@ -286,4 +336,171 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
     }
+
+    private class Server extends Thread{
+
+        //server socket
+        private BluetoothServerSocket bluetoothServerSocket;
+        //prosty konstruktor
+        public Server(){
+
+            try {
+                bluetoothServerSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord(APP_NAME, MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            //klient socket
+            BluetoothSocket bluetoothClientSocket = null;
+
+            Log.d("xxx", "server starting...");
+
+            while (bluetoothClientSocket == null){
+
+                Log.d("xxx", "server listening...");
+
+                try {
+                    bluetoothClientSocket = bluetoothServerSocket.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (bluetoothClientSocket != null){
+
+                    Log.d("xxx", "server success connected!!!");
+
+                    Message message = Message.obtain();
+                    message.what = 0;
+                    handler.sendMessage(message);
+
+                    break;
+
+                }
+
+            }
+
+        }
+    }
+
+    private class Client extends Thread{
+
+        private BluetoothSocket bluetoothSocket;
+        private BluetoothDevice bluetoothDevice;
+
+        //klient w konstruktorze przekazuje dane urządzenia
+
+        public Client(BluetoothDevice bluetoothDevice){
+
+            this.bluetoothDevice = bluetoothDevice;
+
+            try {
+                bluetoothSocket = this.bluetoothDevice.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            //koniec szukania urządzeń
+
+            bluetoothAdapter.cancelDiscovery();
+
+            //łącz z serwerem
+            try {
+                bluetoothSocket.connect();
+                Log.d("xxx", "client connected !!!");
+                Message message = Message.obtain();
+                message.what = 1;
+                handler.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message message) {
+
+            switch (message.what){
+
+                case 0:
+                    status.setText("Połączono!!!");
+                    progressDialog.dismiss();
+                    break;
+
+                case 1:
+                    status.setText("Połączono!!!");
+                    break;
+            }
+
+            return false;
+        }
+    });
+
+    private class Communication extends Thread{
+
+        private BluetoothSocket bluetoothSocket;
+        private InputStream inputStream;
+        private OutputStream outputStream;
+
+        public Communication(BluetoothSocket bluetoothSocket){
+
+            this.bluetoothSocket = bluetoothSocket;
+            try {
+                inputStream = bluetoothSocket.getInputStream();
+                outputStream = bluetoothSocket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        public void writeMessage(byte[] data){
+
+            try {
+                outputStream.write(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            byte[] buffer = new byte[1024];
+            int bytes = 0;
+
+            while (true){
+
+                try {
+                    bytes = inputStream.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (bytes > 0){
+
+                    handler.obtainMessage(1, bytes, -1, buffer).sendToTarget();
+
+                }
+
+            }
+
+        }
+    }
+
 }
